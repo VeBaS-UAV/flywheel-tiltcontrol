6f23d01fb76b7bb7c717be6556526df37fef5826
Matthias Füller <matthias.fueller@fhdw.de>
2021-08-12 14:58:00 +0200

    code formating etc

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,43 @@
 #include <Arduino.h>
 
-const int LED_PIN = LED_BUILTIN;
-
-// input PIN for PWM input signal
-//const int PWM_INPUT_PIN = 2; //attiny
+#if defined(__AVR_ATtiny85__) || defined(__AVR_ATtiny45__)
+const int PWM_INPUT_PIN = PB2;   // attiny PB0
+const int PWM_INPUT_IRQ_PIN = 0; // Int 0 for pin 2 // attiny
+const int PWM_OUTPUT_PIN = PB0;  // attiny PB1
+const float PWM_FREQUENCY = 504; /* in Hz */
+const float PWM_RESOLUTION = 8;  /* in bit */
+const int LED_PIN = PB1;
+#elif defined(ARDUINO_TEENSY31)
 const int PWM_INPUT_PIN = 14; // teensy 3.2
-
-//need to be set on attiny manually
 const int PWM_INPUT_IRQ_PIN = digitalPinToInterrupt(PWM_INPUT_PIN); // teensy
-//const int PWM_INPUT_IRQ_PIN = 0; //Int 0 for pin 2 // attiny
-
-// output PIN for PWM signal
-//const int PWM_OUTPUT_PIN = 1; //attiny
 const int PWM_OUTPUT_PIN = 10;
+const float PWM_FREQUENCY = 488; /* in Hz */
+const float PWM_RESOLUTION = 8;  /* in bit */
+const int LED_PIN = LED_BUILTIN;
+#else // other platforms
+const int PWM_INPUT_PIN = 14; // teensy 3.2
+const int PWM_INPUT_IRQ_PIN = digitalPinToInterrupt(PWM_INPUT_PIN); // teensy
+const int PWM_OUTPUT_PIN = 10;
+const float PWM_FREQUENCY = 488; /* in Hz */
+const float PWM_RESOLUTION = 8;  /* in bit */
+const int LED_PIN = LED_BUILTIN;
+#endif
+
+// #define DEBUG
 
 // valid PWM range definition
-const int PWM_LOW = 1100; /* in us */
+const int PWM_LOW = 1100;    /* in us */
 const int PWM_CENTER = 1500; /* in us */
-const int PWM_HIGH = 2000; /* in us */
+const int PWM_HIGH = 1900;   /* in us */
 
 // define hysteresis for center value
-const int PWM_CTL_HYSTERESIS = 50; /* in us */
-
-// P control value for PWM output based on PWM input-output error
-// I and D control not implemented
-//const float PWM_CTL_P = 0.01f;  /* control factor for PWM control in 1/1000 steps*/
+const int PWM_CTL_HYSTERESIS = 100; /* in us */
 
 // PWM definition based in uc specs
 // usually, 488Hz and 8bit resolution is default
-const float PWM_FREQUENCY = 488; /* in Hz */
-const float PWM_RESOLUTION = 8; /* in bit */
-const int PWM_MAX_DUTY_IN_US = 1.0/PWM_FREQUENCY*1000*1000;
+// const float PWM_FREQUENCY = 488; /* in Hz */
+// const float PWM_RESOLUTION = 8; /* in bit */
+const int PWM_MAX_DUTY_IN_US = 1.0 / PWM_FREQUENCY * 1000 * 1000;
 const float PWM_MAX_DUTY_VALUE = pow(2, PWM_RESOLUTION) - 1;
 
 // delay on each iteration
@@ -39,7 +46,7 @@ const int LOOP_DELAY = 200; /* in ms */
 int pwm_out_duty_in_us = PWM_CENTER; /* in us */
 int mode_counter = 0;
 int mode_out = 0;
-const int MAX_MODE_COUNTER = 5;
+const int MAX_MODE_COUNTER = 3;
 
 int toggle_led = 0;
 
@@ -47,13 +54,21 @@ int toggle_led = 0;
 volatile unsigned long pwm_input_duty_in_us = 0;
 volatile unsigned long pwm_start_time = 0;
 
-
 // enable/disable debug message over serial port
-#define DEBUG
 
 #ifdef DEBUG
+#if defined(__AVR_ATtiny85__) || defined(__AVR_ATtiny45__)
+#include <DigiKeyboard.h>
+#define DEBUG_INIT // DigiKeyboard.begin()
+#define DEBUG_PRINT(x) DigiKeyboard.print(x)
+#define DEBUG_PRINTLN(x) DigiKeyboard.println(x)
+#define DEBUG_FLUSH // DigiKeyboard.refresh();
+#else
+#define DEBUG_INIT Serial.begin(115200)
 #define DEBUG_PRINT(x) Serial.print(x)
 #define DEBUG_PRINTLN(x) Serial.println(x)
+#define DEBUG_FLUSH // DigiKeyboard.refresh();
+#endif
 #else
 #define DEBUG_PRINT(x)
 #define DEBUG_PRINTLN(x)
@@ -62,19 +77,17 @@ volatile unsigned long pwm_start_time = 0;
 // interrupt routing to calculate the duty length in us
 void handle_pwm_input_interrupt() {
 
-  if (digitalRead(PWM_INPUT_PIN))
-  {
+  if (digitalRead(PWM_INPUT_PIN)) {
     pwm_start_time = micros();
   } else {
     pwm_input_duty_in_us = micros() - pwm_start_time;
   }
-
 }
 
 void setup() {
 
 #ifdef DEBUG
-    Serial.begin(115200);
+  DEBUG_INIT;
 #endif
 
   pinMode(LED_PIN, OUTPUT);
@@ -84,67 +97,57 @@ void setup() {
   attachInterrupt(PWM_INPUT_IRQ_PIN, handle_pwm_input_interrupt, CHANGE);
 
   DEBUG_PRINTLN("init completed");
-
 }
 
-int current_pwm_high_in_us()
-{
-  if (pwm_input_duty_in_us >0)
-  {
+int current_pwm_high_in_us() {
+  if (pwm_input_duty_in_us > 0) {
     DEBUG_PRINT("Last time ");
     DEBUG_PRINTLN(pwm_input_duty_in_us);
     int result = pwm_input_duty_in_us;
     pwm_input_duty_in_us = 0;
     return result;
   }
-  
+
   return 0;
 }
 
-int current_input_mode()
-{
+int current_input_mode() {
   int pwm_in_us = current_pwm_high_in_us();
 
   int pwm_c = pwm_in_us - PWM_CENTER;
 
-  if (abs(pwm_c) < PWM_CTL_HYSTERESIS)
-  {
+  if (abs(pwm_c) < PWM_CTL_HYSTERESIS) {
     return 0;
   }
 
-  if (pwm_c > 0)
-  {
+  if (pwm_c > 0) {
     return 1;
   }
 
   return -1;
-
 }
 
 // calculate time in us to duty value based on PWM frequency and resolution
-int convert_usec_to_duty_value(int duty_time_in_us)
-{
-    return map(duty_time_in_us, 0, PWM_MAX_DUTY_IN_US, 0, PWM_MAX_DUTY_VALUE);
+int convert_usec_to_duty_value(int duty_time_in_us) {
+  return map(duty_time_in_us, 0, PWM_MAX_DUTY_IN_US, 0, PWM_MAX_DUTY_VALUE);
 }
 
-int sign(int x)
-{
-  if (x > 0) return 1;
-  if (x < 0) return -1;
+int sign(int x) {
+  if (x > 0)
+    return 1;
+  if (x < 0)
+    return -1;
   return 0;
 }
 
-int set_pwm(int mode_out)
-{
+int set_pwm(int mode_out) {
   int pwm = PWM_CENTER;
 
-  if (mode_out > 0)
-  {
-    pwm =  PWM_HIGH;
+  if (mode_out > 0) {
+    pwm = PWM_HIGH;
   }
-  if (mode_out < 0)
-  {
-    pwm =  PWM_LOW;
+  if (mode_out < 0) {
+    pwm = PWM_LOW;
   }
 
   DEBUG_PRINT("pwm: ");
@@ -159,56 +162,41 @@ int set_pwm(int mode_out)
   return 0;
 }
 
-
 void loop() {
-
-  digitalWrite(LED_PIN, toggle_led);
-  toggle_led = !toggle_led;
-
-  // get current pwm value
-  //int pwm_in_duty_in_us = current_pwm_high_in_us();
 
   int mode_in = current_input_mode();
 
   DEBUG_PRINT("mode_in: ");
   DEBUG_PRINTLN(mode_in);
 
-  if (mode_in == 0)
-  {
+  DEBUG_PRINT("mode_out: ");
+  DEBUG_PRINTLN(mode_out);
+  if (mode_in == 0) {
     mode_counter = MAX_MODE_COUNTER - 1;
   }
 
-  if (mode_in != 0)
-  {
+  if (mode_in != 0) {
 
-    DEBUG_PRINT("mode_out: ");
-    DEBUG_PRINTLN(mode_out);
-
-    if (mode_in != mode_out)
-    {
+    if (mode_in != mode_out) {
       mode_counter++;
     }
 
     DEBUG_PRINT("mode counter: ");
     DEBUG_PRINTLN(mode_counter);
 
-    if(mode_counter > MAX_MODE_COUNTER)
-    {
+    if (mode_counter > MAX_MODE_COUNTER) {
 
       int mode_delta = mode_in - mode_out;
 
-      mode_out = min(max(mode_out + sign(mode_delta),-1), 1);
+      mode_out = min(max(mode_out + sign(mode_delta), -1), 1);
       mode_counter = 0;
 
       DEBUG_PRINT("mode out update: ");
       DEBUG_PRINTLN(mode_out);
-
     }
     set_pwm(mode_out);
-
   }
 
   DEBUG_PRINTLN("####");
   delay(LOOP_DELAY);
-  
 }
